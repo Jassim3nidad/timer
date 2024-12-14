2ff546f6e5feac5f838666136d3a9cc187b37b10
Jassim Trinidad <157487115+Jassim3nidad@users.noreply.github.com>
2024-12-14 17:55:13 +0800

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,13 @@
+import time
+
+timer = int(input("Enter time: "))
+
+
+for x in reversed(range(0, timer)):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
